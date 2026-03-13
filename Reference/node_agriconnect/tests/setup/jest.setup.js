4fb2821e8524aefbd2bdc930940a207d8eaef5b8
abhishek999m <abhishek.malhotra@evervent.in>
2026-03-13 12:02:21 +0530

    removed backend from native repo

--- Reference/node_agriconnect/tests/setup/jest.setup.js
+++
@@ -1,9 +0,0 @@
-const { sequelize } = require('../../src/models');
-
-afterAll(async () => {
-    try {
-        await sequelize.close();
-    } catch (err) {
-        // Ignore teardown noise to avoid hiding actual test assertion failures.
-    }
-});
